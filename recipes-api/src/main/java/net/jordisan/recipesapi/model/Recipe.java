97871db2638840df726a0651c0f45cf036a09481
jordisan <ramsys@gmail.com>
2020-09-25 11:19:49 +0200

    Create Recipe model

---
+++ recipes-api/src/main/java/net/jordisan/recipesapi/model/Recipe.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package net.jordisan.recipesapi.model;
+
+import javax.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+/**
+ * @author jordisan
+ *
+ */
+@Entity
+@Table(name="recipes")
+public class Recipe {
+	
+	private long id;
+	private String title;
+	private String description;
+	private String instructions;
+	private java.net.URL photoUrl;
+	private java.util.Date creationDate;
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	@Column(name = "title", nullable = false)
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	@Column(name = "description", nullable = true)
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	@Column(name = "instructions", nullable = true)
+	public String getInstructions() {
+		return instructions;
+	}
+	public void setInstructions(String instructions) {
+		this.instructions = instructions;
+	}
+	
+	@Column(name = "photo_url", nullable = true)
+	public java.net.URL getPhotoUrl() {
+		return photoUrl;
+	}
+	public void setPhotoUrl(java.net.URL photoUrl) {
+		this.photoUrl = photoUrl;
+	}
+	
+	@Column(name = "creation_date", nullable = false)
+	@CreationTimestamp
+	public java.util.Date getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(java.util.Date creationDate) {
+		this.creationDate = creationDate;
+	}
+}
