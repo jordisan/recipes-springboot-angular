15f23243ba2355e69167140193033b307e3e545e
jordisan <ramsys@gmail.com>
2020-09-30 18:01:09 +0200

    Fix some details in initial data (text formatting; photo url)

--- recipes-api/src/main/resources/data.sql
+++ recipes-api/src/main/resources/data.sql
@@ -4,21 +4,13 @@ INSERT INTO recipes (id, title, description, instructions, photo_url, creation_d
 	'Potato omelette with onion',
 	'Scrape 500g new potatoes or leave the skins on, if you prefer.
 Cut them into thick slices. Chop 1 onion.
-Heat 150ml extra-virgin olive oil in a large frying pan, add the
-potatoes and onion and stew gently, partially covered, for 30
-minutes, stirring occasionally until the potatoes are softened.
-Strain the potatoes and onion through a colander into a large bowl
-(set the strained oil aside).
-Beat 6 eggs, then stir into the potatoes and plenty of salt. Heat a
-little of the strained oil in a smaller pan.
-Tip everything into the pan and cook on a moderate heat, using a
-spatula to shape the omelette into a cushion.
-When almost set, invert on a plate and slide back into the pan and
-cook a few more minutes.
-Invert twice more, cooking the omelette briefly each time and
-pressing the edges to keep the cushion shape. Slide on to a plate
-and cool for 10 minutes before serving.',
-	'https://flexit.es/tortilla-espa%C3%B1ola.jpg',
+Heat 150ml extra-virgin olive oil in a large frying pan, add the potatoes and onion and stew gently, partially covered, for 30 minutes, stirring occasionally until the potatoes are softened.
+Strain the potatoes and onion through a colander into a large bowl (set the strained oil aside).
+Beat 6 eggs, then stir into the potatoes and plenty of salt. Heat a little of the strained oil in a smaller pan.
+Tip everything into the pan and cook on a moderate heat, using a spatula to shape the omelette into a cushion.
+When almost set, invert on a plate and slide back into the pan and cook a few more minutes.
+Invert twice more, cooking the omelette briefly each time and pressing the edges to keep the cushion shape. Slide on to a plate and cool for 10 minutes before serving.',
+	'https://www.196flavors.com/wp-content/uploads/2014/10/Spanish-tortilla-FP.jpg',
 	'2020-02-02 22:22'
 );
 
